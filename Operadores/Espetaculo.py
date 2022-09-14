c3ce4a7465ca953c205aec7eb11bedf33074775a
Guilherme Menezes <107522195+GuilhermeMenezesSilva@users.noreply.github.com>
2022-09-14 19:58:49 -0300

    Programa que calcula o custo que um espetaculo deve alcançar

---
+++ Operadores/Espetaculo.py
@@ -0,0 +1,6 @@
+custoEspetaculo = float(input("Quanto custará para realizar o espetaculo? "))
+valorConvite = float(input("Qual o valor do convite? "))
+
+qtdConvite = custoEspetaculo / valorConvite   
+
+print("Para que o custo seja alcançado, será necessário vender", qtdConvite, "ingressos")
