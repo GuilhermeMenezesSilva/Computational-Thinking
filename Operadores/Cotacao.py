3d13c32342984d9d4e3638230a12f551d8419957
Guilherme Menezes <107522195+GuilhermeMenezesSilva@users.noreply.github.com>
2022-09-14 19:52:43 -0300

    Programa exibe quantos dólares vale os reais que o usuário informou

---
+++ Operadores/Cotacao.py
@@ -0,0 +1,6 @@
+valorDolar = float(input("Qual custa o dolar? "))
+reais = float(input("Quantos reais possui? "))
+
+cotacao = reais/valorDolar
+
+print(cotacao," dolares equivale ao dinheiro que possui em reais")
