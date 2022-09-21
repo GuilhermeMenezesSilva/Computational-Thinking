c202f90d2ec2a6f1d3cf3225ec3f84561bf25cb2
Guilherme Menezes <107522195+GuilhermeMenezesSilva@users.noreply.github.com>
2022-09-21 19:13:59 -0300

    Programa que recebe dois numeros e verifica qual é o maior

---
+++ FluxoDeDecisao/Maior.py
@@ -0,0 +1,7 @@
+num1 = int(input("Digite um numero: "))
+num2 = int(input("Digite outro numero: "))
+
+if num1 > num2:
+    print("O maior é o primeiro numero:", num1)
+else:
+    print("O maior é o segundo numero:", num2)
