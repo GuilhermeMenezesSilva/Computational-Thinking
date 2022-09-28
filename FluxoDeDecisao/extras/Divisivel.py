9244e4d311f656604051a6e265c09213e4b1757a
Guilherme Menezes <107522195+GuilhermeMenezesSilva@users.noreply.github.com>
2022-09-28 17:39:09 -0300

    Programa que verifica se o usuário tem acesso permitido com base na senha digitada

--- FluxoDeDecisao/extras/Divisivel.py
+++
@@ -1,7 +0,0 @@
-numero1 = int(input("Entre com um número inteiro: "))
-numero2 = int(input("Entre com outro número inteiro: "))
-
-if numero1 % numero2 == 0:
-    print("O primeiro número é divisível pelo segundo")
-else:
-    print("O primeiro número não é divisível pelo segundo")
--- FluxoDeDecisao/extras/Iguais.py
+++
@@ -1,7 +0,0 @@
-numero1 = int(input("Entre com um número inteiro: "))
-numero2 = int(input("Entre com outro número inteiro: "))
-
-if numero1 == numero2:
-    print("Números iguais")
-else:
-    print("Números diferentes")
