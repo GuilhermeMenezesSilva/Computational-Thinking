2394ad99042170228b0e62648f83244b933efeb1
Guilherme Menezes <107522195+GuilhermeMenezesSilva@users.noreply.github.com>
2022-10-19 17:55:55 -0300

    Correção do programa

--- Dicionario/Carro.py
+++ Dicionario/Carro.py
@@ -11,20 +11,22 @@ for i in range(5):
 
 def menor_preco():
     menor_valor = car["preço"][0]
-    for i in range (len(car["preço"])):
-        if i < menor_valor:
-            menor_valor = car["preço"][i]
-            indice = i
-            print(f'O menor valor é {car["carro"] [indice]} fabricado em {car["ano"][indice]} que vale R${menor_valor} reais')
+    indice = 0
+    for x in range (len(car["preço"])):
+        if car["preço"][x] < menor_valor:
+            menor_valor = car["preço"][x]
+            indice = x
+            print(f'O menor valor é {car["carro"][indice]} fabricado em {car["ano"][indice]} que vale R${menor_valor} reais')
     return 
 menor_preco()
 
 def maior_preco():
     maior_valor = car["preço"][0]
+    indice = 0
     for j in range (len(car["preço"])):
         if j > maior_valor:
-            maior_valor = car["preço"][i]
+            maior_valor = car["preço"][j]
             indice = j
-            print(f'O menor valor é {car["carro"] [indice]} fabricado em {car["ano"] [indice]} que vale R${maior_valor} reais')
+            print(f'O menor valor é {car["carro"][indice]} fabricado em {car["ano"][indice]} que vale R${maior_valor} reais')
     return 
 maior_preco()
